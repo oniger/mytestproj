e48a23038268247296cb8c5bfd7ab543f1da98da
oniger <40618123+oniger@users.noreply.github.com>
2018-09-29 09:57:25 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "app" {
               sudo wget -P /home/testme --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/jdk-8u181-linux-x64.tar.gz
               sudo docker build -t figlet12 /home/testme/.
               sudo docker build -t figlet13 /home/testme/.
-              sudo ansible-playbook -i /home/testme/hosts -k playbook.yml 
+              sudo ansible-playbook -i /home/testme/hosts -k /home/testme/playbook.yml 
               EOF
 }
 resource "aws_security_group" "web" {
